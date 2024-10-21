2561adea2ef8b8cdf2bde3b0ffc03a279e6bd5e9
Gabriel Caetano <gabrielvscaetano@gmail.com>
2024-10-21 14:38:27 -0300

    add specific products dm

--- main.r
+++ main.r
@@ -4,24 +4,42 @@ library('arules')
 json <- fromJSON('padaria_trab.json')
 
 # limpeza
-list_of_products = unique(unlist(json[2]))
+products = unique(unlist(unique(json[2])))
+
 get_first_word <- function(x) strsplit(x, " ")[[1]][1]
-types <- unique(sapply(list_of_products, get_first_word))
+types <- unique(sapply(products, get_first_word))
+
 
 # transformação
-mat <- matrix(0, nrow = nrow(json), ncol = length(types))
-colnames(mat) <- types
+mat_p <- matrix(0, nrow = nrow(json), ncol = length(products))
+colnames(mat_p) <- products
 for (i in 1:nrow(json)) {
-    produtos <- json$produtos[[i]]
-    prod_types <- unique(sapply(produtos, get_first_word))
-    mat[i,prod_types] <- 1
+    p <- json$produtos[[i]]
+    # prod_types <- unique(sapply(produtos, get_first_word))
+    mat_p[i,p] <- 1
 }
 
+mat_t <- matrix(0, nrow = nrow(json), ncol = length(types))
+colnames(mat_t) <- types
+for (i in 1:nrow(json)) {
+    p <- json$produtos[[i]]
+    pt <- unique(sapply(p, get_first_word))
+    mat_t[i,pt] <- 1
+}
+
+# mineração
 # principais regras
-rules = apriori(mat, parameter = list(supp = 0.2, conf = 0.4, minlen = 2))
-inspect(rules)
+rules_p = apriori(mat_p, parameter = list(supp = 0.06, conf = 0.1, minlen = 2))
+inspect(rules_p)
+rules_t = apriori(mat_t, parameter = list(supp = 0.2, conf = 0.4, minlen = 2))
+inspect(rules_t)
+
 
 # regras que implicam em doces
-rules = apriori(mat, parameter = list(supp = 0.1, conf = 0.2, minlen = 2))
-filtered = subset(rules, rhs %in% "Doce")
+rules_p = apriori(mat_p, parameter = list(supp = 0.03, conf = 0.05, minlen = 2))
+filtered = subset(rules_p, rhs %in% "Doce Goiabada" | rhs %in% "Doce Leite" | rhs %in% "Doce Amendoim")
+inspect(filtered)
+
+rules_t = apriori(mat_t, parameter = list(supp = 0.1, conf = 0.2, minlen = 2))
+filtered = subset(rules_t, rhs %in% "Doce")
 inspect(filtered)
