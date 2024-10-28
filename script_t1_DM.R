328e06bf7782ab63de59c6570570c9f7b9c6e4bb
Brunop10 <142537375+Brunop10@users.noreply.github.com>
2024-10-28 14:51:49 -0300

    Add files via upload

---
+++ script_t1_DM.R
@@ -0,0 +1,42 @@
+library('jsonlite')
+library('arules')
+
+json <- fromJSON('padaria_trab.json')
+
+# limpeza
+products = unique(unlist(unique(json[2])))
+
+get_first_word <- function(x) strsplit(x, " ")[[1]][1]
+types <- unique(sapply(products, get_first_word))
+
+# transformação
+mat_p <- matrix(0, nrow = nrow(json), ncol = length(products))
+colnames(mat_p) <- products
+mat_t <- matrix(0, nrow = nrow(json), ncol = length(types))
+colnames(mat_t) <- types
+
+for (i in 1:nrow(json)) {
+  p <- json$produtos[[i]]
+  pt <- unique(sapply(p, get_first_word))
+  mat_t[i,pt] <- 1
+  mat_p[i,p] <- 1
+}
+
+# mineração
+# principais regras
+rules_p = apriori(mat_p, parameter = list(supp = 0.06, conf = 0.2, minlen = 2))
+rules_p <- sort(rules_p, by = "confidence", decreasing = TRUE)
+inspect(rules_p)
+rules_t = apriori(mat_t, parameter = list(supp = 0.2, conf = 0.4, minlen = 2))
+rules_t <- sort(rules_t, by = "confidence", decreasing = TRUE)
+inspect(rules_t)
+
+
+# regras que implicam em doces
+rules_p = apriori(mat_p, parameter = list(supp = 0.03, conf = 0.1, minlen = 2))
+filtered = subset(rules_p, rhs %in% "Doce Goiabada" | rhs %in% "Doce Leite" | rhs %in% "Doce Amendoim")
+inspect(filtered)
+
+rules_t = apriori(mat_t, parameter = list(supp = 0.1, conf = 0.2, minlen = 2))
+filtered = subset(rules_t, rhs %in% "Doce")
+inspect(filtered)
